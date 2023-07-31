5bc240d5e1a5070f0798239daaa61a85ee41c3ab
Costin <virlan.costin.andrei@gmail.com>
2023-07-31 04:14:27 +0300

    Added initial app delay

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 require("dotenv").config();
 const { setTimeout } = require("timers/promises");
 const RequestBatcher = require("./src/requestBatcher");
+const APP_DELAY_IN_MILISECONDS = 5000; 
 
 const run = async () => {
   const { INTERVAL_DURATION_IN_MILISECONDS } = process.env;
+  await setTimeout(APP_DELAY_IN_MILISECONDS);
 
   while (true) {
     const startTime = Date.now();
