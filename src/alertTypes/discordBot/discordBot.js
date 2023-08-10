8b445a3ac64e45bf6e966b49d42100d6a31c4bdb
Costin <virlan.costin.andrei@gmail.com>
2023-08-10 06:26:15 +0300

    Now adding attachment to discord messages + separated channels for patterns

--- src/alertTypes/discordBot/discordBot.js
+++ src/alertTypes/discordBot/discordBot.js
@@ -1,14 +1,34 @@
 const { REST } = require("@discordjs/rest");
 const { ChannelsAPI } = require("@discordjs/core");
+const path = require("path");
+const { readFileSync } = require("fs");
 
 const discordBot = (() => {
-  const { CHANNEL_ID, DISCORD_TOKEN } = process.env;
+  const { DISCORD_TOKEN } = process.env;
   const rest = new REST({ version: "10" }).setToken(DISCORD_TOKEN);
   const channelsAPI = new ChannelsAPI(rest);
-  const sendAlert = (alertObject) => {
-    const { symbol, timeframe, status } = alertObject;
-    const message = `${symbol}/${timeframe}: ${status}`;
-    channelsAPI.createMessage(CHANNEL_ID, { content: message });
+  const sendAlert = (alertObject, imagePath = null) => {
+    const { channelName, symbol, timeframe, status } = alertObject;
+    let CHANNEL_ID;
+    if (channelName === "Candle Pattern")
+      CHANNEL_ID = process.env.CHANNEL_ID_CANDLE_PATTERN;
+    else if (channelName === "Fisher Transform")
+      CHANNEL_ID = process.env.CHANNEL_ID_FISHER_TRANSFORM;
+    const content = `${symbol}/${timeframe}: ${status}`;
+    if (imagePath) {
+      const data = readFileSync(imagePath);
+      const name = path.basename(imagePath);
+      const files = [
+        {
+          data,
+          name,
+        },
+      ];
+      channelsAPI.createMessage(CHANNEL_ID, {
+        content,
+        files,
+      });
+    } else channelsAPI.createMessage(CHANNEL_ID, { content });
   };
   return { sendAlert };
 })();
