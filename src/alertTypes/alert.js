df48f279fdfad24c7755e517a2b1111b41546522
Costin <virlan.costin.andrei@gmail.com>
2023-08-10 06:25:42 +0300

    Added generate image callback

--- src/alertTypes/alert.js
+++ src/alertTypes/alert.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const discordBot = require("./discordBot");
 
-const Alert = (description, nrOfCandles, fnCheckPattern) => {
+const Alert = (
+  description,
+  nrOfCandles,
+  fnCheckPattern,
+  fnGenerateImage = null
+) => {
   let isTriggered = false;
   let ts = 0;
 
@@ -14,12 +20,20 @@ const Alert = (description, nrOfCandles, fnCheckPattern) => {
     }
   };
 
-  const sendNotification = async (status) => {
-    if (isTriggered) return;
+  const sendNotification = (status, imagePath = null) => {
     isTriggered = true;
-    const { symbol, timeframe } = description;
-    const alertObject = { symbol, timeframe, status };
-    discordBot.sendAlert(alertObject);
+    const { name, symbol, timeframe } = description;
+    const alertObject = { channelName: name, symbol, timeframe, status };
+    discordBot.sendAlert(alertObject, imagePath);
+  };
+
+  const getImagePath = async (rawData) => {
+    const { name, symbol, timeframe } = description;
+    const nameTrimmed = name.replace(/\s/g, '');
+    const imageName = `/${nameTrimmed}${symbol}${timeframe}.png`;
+    const imagePath = path.join(__dirname, `/chartPNGs${imageName}`);
+    await fnGenerateImage(rawData, imagePath);
+    return imagePath;
   };
 
   const evaluateAndNotify = async (rawData) => {
@@ -27,7 +41,13 @@ const Alert = (description, nrOfCandles, fnCheckPattern) => {
     const lastCandle = candles.slice(-1);
     updateStatusAndTimestamp(lastCandle);
     const status = fnCheckPattern(candles, description);
-    if (status) await sendNotification(status);
+    if (status && !isTriggered)
+      if (fnGenerateImage) {
+        const imagePath = await getImagePath(rawData);
+        sendNotification(status, imagePath);
+      }
+      else
+        sendNotification(status)
   };
   return { evaluateAndNotify };
 };
